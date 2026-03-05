501df622bcccffa712fcebdafd87a5f7e48f3e6c
edouardauger2-art <edouard.auger2@gmail.com>
2026-03-05 16:36:53 -0500

    Modification de solver pour résoudre l'équation instationnaire avec le nouveau terme source

--- Devoir 2/main.py
+++ Devoir 2/main.py
@@ -3,7 +3,7 @@ Fichier roulant le code nécessaire pour le devoir.
 """
 import os
 import numpy as np
-from src.solver.solver import first_order, second_order, analytique
+from src.solver.solver import first_order, second_order
 from src.postprocessing.plotter import (plotter, graph_error_log,
                                         graph_error_radius, print_convergence_table)
 from src.verif.error import norm_l1, norm_l2, norm_infinity
@@ -14,91 +14,94 @@ params = {
     "RO": 0.5,
     "S": 2e-8,
     "D_EFF": 1e-10,
-    "CE": 20
+    "CE": 20,
+    "K": 4e-9,
+    "DT": 0.01,
+    "TF": 100
 }
 
-N_POINTS_MIN=10
-N_POINTS_MAX=10000
-NB_SIMULATION=5
+# N_POINTS_MIN=10
+# N_POINTS_MAX=10000
+# NB_SIMULATION=5
 
-if __name__ == "__main__":
-    # Créer le dossier results s'il n'existe pas
-    os.makedirs("results", exist_ok=True)
+# if __name__ == "__main__":
+#     # Créer le dossier results s'il n'existe pas
+#     os.makedirs("results", exist_ok=True)
 
-    list_n_points = np.logspace(
-        np.log10(N_POINTS_MIN),
-        np.log10(N_POINTS_MAX),
-        NB_SIMULATION,
-        dtype=int)
+#     list_n_points = np.logspace(
+#         np.log10(N_POINTS_MIN),
+#         np.log10(N_POINTS_MAX),
+#         NB_SIMULATION,
+#         dtype=int)
 
-    dr_list = []
-    l1_list_1, l2_list_1, linf_list_1 = [], [], []
-    l1_list_2, l2_list_2, linf_list_2 = [], [], []
+#     dr_list = []
+#     l1_list_1, l2_list_1, linf_list_1 = [], [], []
+#     l1_list_2, l2_list_2, linf_list_2 = [], [], []
 
-    for n_points in list_n_points:
-        discretization_1, concentration_1 = first_order(params, n_points)
-        discretization_2, concentration_2 = second_order(params,n_points)
-        discretization_a, concentration_a = analytique(params,n_points)
+#     for n_points in list_n_points:
+#         discretization_1, concentration_1 = first_order(params, n_points)
+#         discretization_2, concentration_2 = second_order(params,n_points)
+#         discretization_a, concentration_a = analytique(params,n_points)
 
-        discretization = np.linspace(params["RI"], params["RO"], n_points)
-        dr = discretization[1] - discretization[0]
-        dr_list.append(dr)
+#         discretization = np.linspace(params["RI"], params["RO"], n_points)
+#         dr = discretization[1] - discretization[0]
+#         dr_list.append(dr)
 
-        # First order norms
-        l1_list_1.append(norm_l1(concentration_1, concentration_a))
-        l2_list_1.append(norm_l2(concentration_1, concentration_a))
-        linf_list_1.append(norm_infinity(concentration_1, concentration_a))
+#         # First order norms
+#         l1_list_1.append(norm_l1(concentration_1, concentration_a))
+#         l2_list_1.append(norm_l2(concentration_1, concentration_a))
+#         linf_list_1.append(norm_infinity(concentration_1, concentration_a))
 
-        # Second order norms
-        l1_list_2.append(norm_l1(concentration_2, concentration_a))
-        l2_list_2.append(norm_l2(concentration_2, concentration_a))
-        linf_list_2.append(norm_infinity(concentration_2, concentration_a))
+#         # Second order norms
+#         l1_list_2.append(norm_l1(concentration_2, concentration_a))
+#         l2_list_2.append(norm_l2(concentration_2, concentration_a))
+#         linf_list_2.append(norm_infinity(concentration_2, concentration_a))
 
-        # Plots first and last iterations
-        if n_points in (list_n_points[0], list_n_points[-1]):
-            plotter(params, discretization_1,
-                    concentration_1, discretization_a,
-                    concentration_a, order=1,
-                    save_path=f"results/numeric_vs_analytic_order_1_npoints_{n_points}.png")
+#         # Plots first and last iterations
+#         if n_points in (list_n_points[0], list_n_points[-1]):
+#             plotter(params, discretization_1,
+#                     concentration_1, discretization_a,
+#                     concentration_a, order=1,
+#                     save_path=f"results/numeric_vs_analytic_order_1_npoints_{n_points}.png")
 
-            plotter(params, discretization_2,
-                    concentration_2, discretization_a,
-                    concentration_a, order=2,
-                    save_path=f"results/numeric_vs_analytic_order_2_npoints_{n_points}.png")
+#             plotter(params, discretization_2,
+#                     concentration_2, discretization_a,
+#                     concentration_a, order=2,
+#                     save_path=f"results/numeric_vs_analytic_order_2_npoints_{n_points}.png")
     
-    # Vérification de symétrie
-    gen_symetrie(params)
+#     # Vérification de symétrie
+#     gen_symetrie(params)
     
-    # Plots
-    graph_error_log(params, dr_list, l1_list_1, l2_list_1, linf_list_1,
-                    order=1, save_path="results/error_log_order_1.png")
+#     # Plots
+#     graph_error_log(params, dr_list, l1_list_1, l2_list_1, linf_list_1,
+#                     order=1, save_path="results/error_log_order_1.png")
 
-    graph_error_log(params, dr_list, l1_list_2, l2_list_2, linf_list_2,
-                    order=2, save_path="results/error_log_order_2.png")
+#     graph_error_log(params, dr_list, l1_list_2, l2_list_2, linf_list_2,
+#                     order=2, save_path="results/error_log_order_2.png")
 
-    graph_error_radius(params, discretization, concentration_1,
-                       concentration_a, order=1,
-                       save_path="results/error_radius_order_1.png")
+#     graph_error_radius(params, discretization, concentration_1,
+#                        concentration_a, order=1,
+#                        save_path="results/error_radius_order_1.png")
 
-    graph_error_radius(params, discretization, concentration_2,
-                       concentration_a, order=2,
-                       save_path="results/error_radius_order_2.png")
+#     graph_error_radius(params, discretization, concentration_2,
+#                        concentration_a, order=2,
+#                        save_path="results/error_radius_order_2.png")
 
-    # Convergence tables
-    print_convergence_table(list_n_points, dr_list, l1_list_1,
-                            order=1, label="L1")
+#     # Convergence tables
+#     print_convergence_table(list_n_points, dr_list, l1_list_1,
+#                             order=1, label="L1")
 
-    print_convergence_table(list_n_points, dr_list, l2_list_1,
-                            order=1, label="L2")
+#     print_convergence_table(list_n_points, dr_list, l2_list_1,
+#                             order=1, label="L2")
 
-    print_convergence_table(list_n_points, dr_list, linf_list_1,
-                            order=1, label="LInf")
+#     print_convergence_table(list_n_points, dr_list, linf_list_1,
+#                             order=1, label="LInf")
 
-    print_convergence_table(list_n_points, dr_list, l1_list_2,
-                            order=2, label="L1")
+#     print_convergence_table(list_n_points, dr_list, l1_list_2,
+#                             order=2, label="L1")
 
-    print_convergence_table(list_n_points, dr_list, l2_list_2,
-                            order=2, label="L2")
+#     print_convergence_table(list_n_points, dr_list, l2_list_2,
+#                             order=2, label="L2")
 
-    print_convergence_table(list_n_points, dr_list, linf_list_2,
-                            order=2, label="LInf")
+#     print_convergence_table(list_n_points, dr_list, linf_list_2,
+#                             order=2, label="LInf")
--- Devoir 2/src/solver/solver.py
+++ Devoir 2/src/solver/solver.py
@@ -15,31 +15,38 @@ def first_order(params:dict, n_points=100):
     s = params["S"]
     ce = params["CE"]
     d_eff = params["D_EFF"]
+    K = params["K"]
+    dt = params["DT"]
+    tf = params["TF"]
+
     discretization = np.linspace(ri, ro, n_points)
     dr = discretization[1] - discretization[0]
+    concentration_vect = np.zeros(n_points)
+    temps = 0
 
-    a = np.zeros((n_points, n_points))
-    b = np.ones(n_points) * s
+    while temps < tf:
+        a = np.zeros((n_points, n_points))
+        b = np.zeros(n_points) 
 
-    if ri == 0:
         a[0, 0] = -1
         a[0, 1] = 1
         b[0] = 0
 
-    else:
-        b[0] = ce
-        a[0, 0] = 1
+        a[-1, -1] = 1
+        b[-1] = ce
 
-    a[-1, -1] = 1
-    b[-1] = ce
+        lam = d_eff*dt/(dr**2)
+        for i in range(1, n_points - 1):
+            r_i = discretization[i]
+            mu = d_eff*dt/(r_i*dr)
 
-    for i in range(1, n_points - 1):
-        r_i = discretization[i]
-        a[i, i-1] = d_eff / (dr**2)
-        a[i, i] = -2 * d_eff / (dr**2) - d_eff / (r_i * dr)
-        a[i, i+1] = d_eff / (r_i * dr) + d_eff / (dr**2)
+            a[i, i-1] = -lam
+            a[i, i] = 1 + K*dt + 2*lam + mu
+            a[i, i+1] = - lam - mu
+            b[i] = concentration_vect[i]
 
-    concentration_vect = np.linalg.solve(a, b)
+        concentration_vect = np.linalg.solve(a, b)
+        temps += dt
 
     return discretization, concentration_vect
 
@@ -56,48 +63,41 @@ def second_order(params:dict, n_points=100):
     s = params["S"]
     ce = params["CE"]
     d_eff = params["D_EFF"]
+    K = params["K"]
+    dt = params["DT"]
+    tf = params["TF"]
+
+
+    temps = 0
     discretization = np.linspace(ri, ro, n_points)
     dr = discretization[1] - discretization[0]
+    concentration_vect = np.zeros(n_points)
 
-    a = np.zeros((n_points, n_points))
-    b = np.ones(n_points) * s
+    while temps < tf:
+        
+        a = np.zeros((n_points, n_points))
+        b = np.zeros(n_points)
 
-    if ri == 0:
         a[0, 0] = -3
         a[0, 1] = 4
         a[0, 2] = -1
         b[0] = 0.0
 
-    else:
-        b[0] = ce
-        a[0, 0] = 1
 
-    a[-1, -1] = 1
-    b[-1] = ce
+        a[-1, -1] = 1
+        b[-1] = ce 
 
-    for i in range(1, n_points - 1):
-        r_i = discretization[i]
-        a[i, i-1] = d_eff / (dr**2) - d_eff / (2 * r_i * dr)
-        a[i, i] = -2 * d_eff / (dr**2)
-        a[i, i+1] = d_eff / (2 * r_i * dr) + d_eff / (dr**2)
+        lam = d_eff*dt/(dr**2)
 
-    concentration_vect = np.linalg.solve(a, b)
+        for i in range(1, n_points - 1):
+            r_i = discretization[i]
+            mu = d_eff*dt/(2*r_i*dr)
 
-    return discretization, concentration_vect
-
-
-def analytique(params:dict, n_points=100):
-    """
-    Solution analytique du problème de diffusion.
-
-    :param params: paramètres du problème
-    :param n_points: nombre de noeuds
-    """
-    ri = params["RI"]
-    ro = params["RO"]
-    s = params["S"]
-    ce = params["CE"]
-    d_eff = params["D_EFF"]
-    discretization = np.linspace(ri, ro, n_points)
-    concentration_vect = 0.25 * s * ro**2 * (discretization**2 / ro**2 - 1) / d_eff + ce
+            a[i, i-1] = -lam + mu
+            a[i, i] = 1 + K*dt + 2*lam
+            a[i, i+1] = -lam - mu
+            b[i] = concentration_vect[i]
+            
+        concentration_vect = np.linalg.solve(a, b)
+        temps += dt
     return discretization, concentration_vect
